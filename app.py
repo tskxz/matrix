fe85e87c3f92161109aabe538bdf409f803b96e9
Dark1ness <frankhorn99@gmail.com>
2026-01-20 10:42:47 +0000

    Adding simple get routing

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from core.matrix import Matrix
 
 app = Flask(__name__)
 
+# App Routes
+
 @app.route('/')
 def index():
     """Página inicial/menu."""
@@ -11,42 +13,53 @@ def index():
 @app.route('/sum-sub', methods=['GET', 'POST'])
 def sum_sub():
     """Soma/Subtração de matrizes."""
-    return render_template('sum_sub.html')
+    if request.method == 'GET':
+        return render_template('sum_sub.html')
 
 @app.route('/multiply', methods=['GET', 'POST'])
 def multiply():
     """Multiplicação de matrizes."""
-    return render_template('multiply.html')
+    if request.method == 'GET':
+        return render_template('multiply.html')
 
 @app.route('/transpose', methods=['GET', 'POST'])
 def transpose():
     """Transpor uma Matriz"""
-    return render_template('transpose.html')
+    if request.method == 'GET':
+        return render_template('transpose.html')
 
 @app.route('/scalar', methods=['GET', 'POST'])
 def scalar():
     """Multiplicação de matriz por escalar."""
-    return render_template('scalar.html')
+    if request.method == 'GET':
+        return render_template('scalar.html')
 
 @app.route('/determinant', methods=['GET', 'POST'])
 def determinant():
     """Cálculo de determinante."""
-    return render_template('determinant.html')
+    if request.method == 'GET':
+        return render_template('determinant.html')
 
 @app.route('/inverse', methods=['GET', 'POST'])
 def inverse():
     """Cálculo de matriz inversa."""
-    return render_template('inverse.html')
+    if request.method == 'GET':
+        return render_template('inverse.html')
 
 @app.route('/encrypt', methods=['GET', 'POST'])
 def encrypt():
     """Criptografia de mensagens usando multiplicação matricial."""
-    return render_template('encrypt.html')
+    if request.method == 'GET':
+        return render_template('encrypt.html')
 
 @app.route('/decrypt', methods=['GET', 'POST'])
 def decrypt():
     """Descriptografia de mensagens usando multiplicação matricial."""
-    return render_template('decrypt.html')
+    if request.method == 'GET':
+        return render_template('decrypt.html')
+
+# Api Routes
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
